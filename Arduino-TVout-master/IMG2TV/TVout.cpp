32caf169da1316b3ef4b0d49c39dcfbcedfe9575
yukonnor <caschm@gmail.com>
2016-10-28 22:20:43 -0700

    comment out all tri

--- Arduino-TVout-master/IMG2TV/TVout.cpp
+++ Arduino-TVout-master/IMG2TV/TVout.cpp
@@ -521,20 +521,22 @@ void TVout::draw_rect(uint8_t x0, uint8_t y0, uint8_t w, uint8_t h, char c, char
  *	fc:
  *		The fill color of the triangle..not sure how to do this yet
 */
-void TVout::draw_tri(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, uint8_t x3, uint8_t y3, char c, char fc) {
+
+/*void TVout::draw_tri(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, uint8_t x3, uint8_t y3, char c, char fc) {
 	unsigned char i = y1;
 	float xrow0 = (x1 + (((y1-i)*(x1-x3))/(y1+y3)));
 	float xrow1 = (x1 + (((y1-i)*(x1-x2))/(y1+y2)));
 	
 	/*if (fc != -1) {
 		for (i; i < (min(y2, y3)); i--)
-			draw_row(i,xrow0,xrow1,fc);*/
+			draw_row(i,xrow0,xrow1,fc);
 	}
 	draw_line(x1,y1,x2,y2,c);
 	draw_line(x2,y2,x3,y3,c);
 	draw_line(x1,y1,x3,y3,c);
 } // end of draw_tri
-
+*/
+	
 /* draw a circle given a coordinate x,y and radius both filled and non filled.
  *
  * Arguments:
